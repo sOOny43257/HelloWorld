99377f700c12157deaeac28961715d2404349894
sOOny43257 <a100052156@gmail.com>
2015-07-02 19:38:00 +0800

    First Demo
    15/07/02

---
+++ Hello/src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("FUCK THIS WORLD");
+		Scanner in=new Scanner(System.in);
+		
+		double x=9.997;
+		int num=(int)x;
+		int num_1=(int)Math.round(x);
+		System.out.println(num);
+		System.out.println(num_1);
+		String s="Hello";
+		String s_new=s.substring(0,3)+"p";
+		System.out.println(s_new);
+	}
+}
